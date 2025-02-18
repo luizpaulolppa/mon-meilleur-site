7c13a66e51a5571d27f7a3acb74c373ab199a786
Luiz Paulo Pilegi de Almeida <luizpilegilppa@gmail.com>
2025-02-17 23:22:03 -0300

    adding SpeedInsights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import "./globals.css";
 
@@ -27,6 +28,7 @@ export default function RootLayout({
         className={`${poppins.variable} antialiased`}
       >
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
